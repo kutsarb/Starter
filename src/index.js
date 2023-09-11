ac116899fd2c49f9e977204a3834157e83493d70
1 <kutsarb@gmail.com>
2023-09-11 00:07:43 -0400

    made loads and home page. Made off canvas nav bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import CustomNavbar from './Navbar';
-import Nav2 from './nav2';
+import React from 'react';
+import { Container, Row, Col, Button, Navbar } from 'react-bootstrap';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Nav2 from './pages/nav2';
+import Dash from './pages/Dash';
+import Loads from './pages/Loads';
 
-ReactDOM.render(<Nav2 />, document.getElementById('navB'));
+
+function App() {
+    return (
+
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dash />} />
+                <Route index element={<Dash />} />
+                <Route path="/Loads" element={<Loads />} />
+            </Routes>
+        </BrowserRouter>
+
+
+    );
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Nav2 />, document.getElementById('navB'));
