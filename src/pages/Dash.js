5000a222122b3e1988f3009af553ac1d3d974a0a
kutsarb <kutsarb@gmail.com>
2023-09-11 18:49:16 -0400

    idk, updates

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -5,28 +5,44 @@ const Dash = () => {
   return (
 
 <div>
-      <div className="body row mt-3">
+      <div className="body row">
 
-        <div className="col-sm-6">
+        <div className="col-sm-3">
           <div id="" className="card" >
             <div className="card-body">
-              <h1 className='card-title text-center'>Welcome to My React Bootstrap Site</h1>
-              <p className="card-text text-center">This is a simple example of using React with Bootstrap.</p>
+              <h4 className='card-title text-center'>Open Loads</h4>
+              <h2 className="card-text text-center Red" >13</h2>
               <div className="d-grid gap-2 d-md-flex justify-content-md-center">
-                <button className="btn btn-primary me-md-2" type="button">Button</button>
-                <button className="btn btn-primary" type="button">Button</button>
               </div>
             </div>
           </div>
         </div>
-        <div className="col-sm-6">
+        <div className="col-sm-3">
           <div id="" className="card" >
             <div className="card-body">
-              <h1 className='card-title text-center'>Welcome to My React Bootstrap Site</h1>
+            <h4 className='card-title text-center'>Welcome to My React Bootstrap Site</h4>
               <p className="card-text text-center">This is a simple example of using React with Bootstrap.</p>
               <div className="d-grid gap-2 d-md-flex justify-content-md-center">
-                <button className="btn btn-primary me-md-2" type="button">Button</button>
-                <button className="btn btn-primary" type="button">Button</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-3">
+          <div id="" className="card" >
+            <div className="card-body">
+            <h4 className='card-title text-center'>Welcome to My React Bootstrap Site</h4>
+              <p className="card-text text-center">This is a simple example of using React with Bootstrap.</p>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-center">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-3">
+          <div id="" className="card" >
+            <div className="card-body">
+            <h4 className='card-title text-center'>Welcome to My React Bootstrap Site</h4>
+              <p className="card-text text-center">This is a simple example of using React with Bootstrap.</p>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-center">
               </div>
             </div>
           </div>
--- src/pages/nav2.js
+++ src/pages/nav2.js
@@ -5,70 +5,140 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-
-
+import Dropdown from 'react-bootstrap/Dropdown';
+import { useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import AccordionContext from 'react-bootstrap/AccordionContext';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import Tab from 'react-bootstrap/Tab';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
 
 const Nav2 = () => {
   return (
     <>
-    {[false].map((expand) => (
-      <Navbar key={expand} expand={expand} variant="dark" className="bg-dark mb-3" data-bs-theme="dark">
-        <Container fluid>
-            <Navbar.Brand className='nItem' href="/">TrackD & TruckD</Navbar.Brand>
+      {[false].map((expand) => (
+
+        <Navbar key={expand} expand={expand} className="bg-dark mb-3  border" data-bs-theme="dark">
+          <Container fluid className='navCont'>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-              className="bg-dark mb-3 " data-bs-theme="dark"
+              placement="start"
+              className="bg-dark mb-3" data-bs-theme="dark"
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
                   Menu
                 </Offcanvas.Title>
+
               </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                
-                <Nav.Link href='/'>Home</Nav.Link>
+              <Tab.Container id="left-tabs-example">
+                <Offcanvas.Body >
 
-                
-                <NavDropdown
-                    title="Loads"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                    
-                  >
-                <NavDropdown.Item  href="/openLoads">Open</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item  href="/dispatchedLoads">Dispatched</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item  href="/deliveredLoads">Delivered</NavDropdown.Item>
-                
-                </NavDropdown>
+                  <Nav variant="pills" className="flex-column">
 
 
-                <Nav.Link href="#2">Services</Nav.Link>
-
-                <Nav.Link href="#3">Contact</Nav.Link>
 
 
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
+                    <Nav.Link eventKey='' href='/'>Home</Nav.Link>
+
+                    <NavDropdown
+                      title="Loads"
+                      id={`offcanvasNavbarDropdown-expand-${expand}`}
+                    >
+                      <NavDropdown.Item className='second' eventKey="/Loads" href="/Loads">Loads Dashboard</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item className='third' eventKey="/openLoads" href="/openLoads">Open</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item className='fourth' href="/dispatchedLoads">Dispatched</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item className='fifth' href="/deliveredLoads">Delivered</NavDropdown.Item>
+
+                    </NavDropdown>
+
+
+
+
+
+
+
+
+                    <NavDropdown
+                      title="Dispatch"
+                      id={`offcanvasNavbarDropdown-expand-${expand}`}
+                    >
+                      <NavDropdown.Item href="/dispatchedLoads">Available Drivers</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="/deliveredLoads">Dispatched</NavDropdown.Item>
+
+                    </NavDropdown>
+
+
+
+
+
+
+                    <Nav.Link href="#3">Drivers</Nav.Link>
+
+
+
+
+
+                    <NavDropdown
+                      title="Accounting"
+                      id={`offcanvasNavbarDropdown-expand-${expand}`}
+                      href="/Loads"
+                    >
+                      <NavDropdown.Item href="/dispatchedLoads">To Dispatch</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="/deliveredLoads">Dispatched</NavDropdown.Item>
+
+                    </NavDropdown>
+
+                  </Nav>
+
+                  <Form className="d-flex">
+                    <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                  </Form>
+                </Offcanvas.Body>
+              </Tab.Container>
             </Navbar.Offcanvas>
+
+            <Navbar.Brand className='nItem ' href="/">
+              <Container>
+
+                <Row>
+
+                  <Col>
+                    <h3 placement="start">TrackD & TruckD </h3>
+                  </Col>
+
+                  <Col>
+                    <Image className=" pl-5" placement="end" src="http://www.w3.org/2000/svg" rounded />
+                  </Col>
+
+                </Row>
+
+              </Container>
+            </Navbar.Brand>
+
+
+
+
           </Container>
         </Navbar>
-        ))}
-        </>
-    );
-    }
+      ))}
+    </>
+  );
+}
 
 export default Nav2;
